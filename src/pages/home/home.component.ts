d11388b110a78e5bd479d6b9453ba9bed36d4a91
Milton Cruz <mr.cruzm@gmail.com>
2026-01-22 10:57:15 -0800

    Added home and admin pages

---
+++ src/pages/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.template.html",
+  styleUrls: ["./home.styles.css"],
+})
+export class HomeComponent {   }
