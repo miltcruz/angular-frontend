6b1a79c34e9bdd8f9d8bae92ad1385e0742913cf
Milton Cruz <mr.cruzm@gmail.com>
2026-02-17 11:00:02 -0800

    Updated API service and used it in Home component

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { InputFormComponent } from "../../component/input-form/input-form";
+import { ApiService } from "../../services/api.service";
 
 @Component({
   selector: "app-home",
@@ -7,4 +8,25 @@ import { InputFormComponent } from "../../component/input-form/input-form";
   styleUrls: ["./home.styles.css"],
   imports: [InputFormComponent],
 })
-export class HomeComponent {   }
+export class HomeComponent implements OnInit { 
+
+  constructor(public api: ApiService) {}
+
+  ngOnInit(): void {
+      this.loadObjects();
+  }
+
+  loadObjects() {
+    this.api.getObjects().subscribe({
+      next: (response) => {
+        console.log(response);
+        this.api.loading.set(false);
+      },
+      error: (err) => {
+        console.error(err);
+        this.api.error.set('Failed to load objects');
+        this.api.loading.set(false);
+      }
+    });
+  }
+}
--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable, signal } from "@angular/core";
 import { APIResponse } from "../models/api.object";
+import { Observable } from "rxjs";
 
 
 @Injectable({ providedIn: 'root' })
@@ -11,13 +12,21 @@ readonly error = signal<string | null>(null);
 
 constructor(private http: HttpClient) {}
     
-    getObjects() {
+    getObjects(): Observable<APIResponse[]> {
+        this.error.set(null);
         this.loading.set(true);
         return this.http.get<APIResponse[]>(this.base);
     }
     
     getObjectById(id: string) {
+        this.error.set(null);
         this.loading.set(true);
         return this.http.get<APIResponse>(`${this.base}/${id}`);
+    }
+
+    deleteObject(id: string) {
+        this.error.set(null);
+        this.loading.set(true);
+        return this.http.delete(`${this.base}/${id}`);
     }
 }
