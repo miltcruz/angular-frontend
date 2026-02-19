6900aa31fcb63a5c928a824e3d9727bed9328634
Milton Cruz <mr.cruzm@gmail.com>
2026-02-19 11:03:11 -0800

    Listing out items and filter out based on seearch term

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from "@angular/core";
 import { InputFormComponent } from "../../component/input-form/input-form";
 import { ApiService } from "../../services/api.service";
+import { APIResponse } from "../../models/api.object";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.template.html",
   styleUrls: ["./home.styles.css"],
-  imports: [InputFormComponent],
+  imports: [InputFormComponent, FormsModule],
 })
 export class HomeComponent implements OnInit { 
+
+  objects: APIResponse[] = [];
+  query: string = '';
+ 
 
   constructor(public api: ApiService) {}
 
@@ -16,10 +22,18 @@ export class HomeComponent implements OnInit {
       this.loadObjects();
   }
 
+  get FilteredObjects(): APIResponse[] {
+    if (!this.query) {
+      return this.objects;
+    }
+    return this.objects.filter(obj => obj.name.toLowerCase().includes(this.query.toLowerCase().trim()));
+  }
+
   loadObjects() {
     this.api.getObjects().subscribe({
       next: (response) => {
         console.log(response);
+        this.objects = response as APIResponse[];
         this.api.loading.set(false);
       },
       error: (err) => {
